f28127f0fd08c9b01f32ff4cae18cd2ae45aa599
penpen09 <ar-el.mbl.10@outlook.jp>
2020-09-02 16:16:17 +0900

    add:ログイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sessions/new', only: [:new, :create, :destroy]
 end
