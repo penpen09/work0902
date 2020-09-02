3af5e08cd11a84a177aad45f8a328376b21eaa2e
penpen09 <ar-el.mbl.10@outlook.jp>
2020-09-02 16:57:32 +0900

    修正２

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new', only: [:new, :create, :destroy]
   resources :blogs
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sessions/new', only: [:new, :create, :destroy]
 end
