58c0685d4d52ec5ad69840f93b8a95deeeda26d8
tomoo sasaabe <sasa.sakurajima@gmail.com>
2020-09-02 17:33:57 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sessions/new', only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 end
